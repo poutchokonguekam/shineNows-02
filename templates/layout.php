cfc3e0cba6524cbdc2195234cca222cfe4bf5f01
poutchokonguekam <poutchoko48@yahoo.fr>
2025-11-21 23:31:07 +0100

    Update metallic card design (#d9d8d4), spacing improvements, favicon added

--- templates/layout.php
+++ templates/layout.php
@@ -24,6 +24,7 @@ if (!isset($currentSlug)) {
     <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="icon" type="image/png" href="/assets/images/favicon.png">
 </head>
 <body>
 
